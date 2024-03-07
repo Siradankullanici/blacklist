725a7879a9ebc2c6d7c0d6392bd574a1d99aa524
Savičs <exerra@exerra.xyz>
2024-03-07 18:29:09 +0000

    api v2 -> api v3

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 const url = {
-    domains: "https://api.exerra.xyz/v2/scam/all/domains",
-    links: "https://api.exerra.xyz/v2/scam/all/links"
+    domains: "https://api.exerra.xyz/v3/phishing/all/domains",
+    links: "https://api.exerra.xyz/v3/phishing/all/urls"
 }
 
 const domainReq = await fetch(url.domains)
 const linksReq = await fetch(url.links)
 
-if (domainReq.status !== 200) throw Error("Exerra Phishing API /all/domains status !== 200")
-if (linksReq.status !== 200) throw Error("Exerra Phishing API /all/links status !== 200")
+if (domainReq.status !== 200) throw Error("Exerra Phishing API v3 /all/domains status !== 200")
+if (linksReq.status !== 200) throw Error("Exerra Phishing API v3 /all/urls status !== 200")
 
 const domains = await domainReq.json() as { status: 200 | 400 | 500, data: [string] }
 const links = await linksReq.json() as { status: 200 | 400 | 500, data: [string] }
